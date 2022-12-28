3e5c6f1c901f2a99cd2eb31ce4979ab123673361
Сергей Каращук <kspost01@gmail.com>
2022-12-29 01:19:43 +0200

    draft version 29.12

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStartEl = document.querySelector('.js-btnStart');
+const btnStopEl = document.querySelector('.js-btnStop');
 
+btnStartEl.addEventListener('click', onStartClick);
+function onStartClick() {
+    timerId = setInterval(() => {
+        btnStartEl.disabled = true;
+        document.body.style.background = getRandomHexColor();
+    console.log(document.body.style.background);
+      }, 1000);
+}
+
+btnStopEl.addEventListener('click', onStopClick);
+function onStopClick() {
+    clearInterval(timerId);
+    btnStartEl.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
