f6882e4fdeabf070bdb082e083cb0cee345149cb
Сергей Каращук <kspost01@gmail.com>
2022-12-31 04:08:58 +0200

    draft version 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,32 +2,6 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-const input = document.querySelector('#datetime-picker');
-const fp = flatpickr(input);
-
-const btnStart = document.querySelector('.js-btnStart');
-// btnStart.addEventListener('click', startTimer);
-input.addEventListener('change', getDayX)
-
-
-const startTime = Date.now();
-console.log('startTime:', startTime);
-
-
-setInterval(() => {
-const currentTime = Date.now();
-const ms = currentTime - startTime;
-const timeComponents = convertMs(ms) 
-console.log('timeComponents:', timeComponents);  
-}, 1000);
-
-function getDayX() {
-    const selectedDate = Date.now(input.value);
-    console.log('selectedDate:', selectedDate);
-  };
-
-
-
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 }
@@ -47,7 +21,62 @@ function convertMs(ms) {
     const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
     const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-  
+
     return { days, hours, minutes, seconds };
   }
+
+const input = document.querySelector('#datetime-picker');
+const timeDays = document.querySelector('[data-days]');
+const timeHours = document.querySelector('[data-hours]');
+const timeMinutes = document.querySelector('[data-minutes]');
+const timeSeconds = document.querySelector('[data-seconds]');
+
+const options = {
+    timerId: null,
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+    selectedDates = Date.now(input.value);
+      console.log(selectedDates[0]);
+    },
+  };
+
+    const fp = flatpickr(input, options);
+    const btnStart = document.querySelector('.js-btnStart');
+
+// btnStart.addEventListener('click', startTimer);
+    input.addEventListener('change', getDayX)
+
+function getDayX() {
+    timerId = setInterval(() => {
+        let backTime = fp.selectedDates[0] - new Date();
+        const { days, hours, minutes, seconds } = convertMs(backTime);
+        // console.log('backTime:', backTime); 
+        console.log('${days} : ${hours} : ${minutes} : ${seconds}'); 
+
+// let currentTime = backTime;
+//         let timeDay = currentTime.getDay();
+//         let timeHours = currentTime.getHours();
+//         let timeMinutes = currentTime.getMinutes();
+//         let timeSeconds = currentTime.getSeconds();
+
+
+
+        if (backTime <= 0) {
+            clearInterval(timerId);
+            console.log(`Interval with id ${timerId} has stopped!`);
+          }
+        }, 1000);
+}
+
+function updateFaseTimer({ days, hours, minutes, seconds }){
+    timeDays.textContent = '${days}';
+    timeHours.textContent = '${hours}';
+    timeMinutes.textContent = '${minutes}';
+    timeSeconds.textContent = '${seconds}'; 
+}
+
+
   
