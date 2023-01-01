a1ec1b1d60be2da89769e40a317b4ef6c11ec7b5
Сергей Каращук <kspost01@gmail.com>
2023-01-01 17:37:55 +0200

    draft version 4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,13 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+
 
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
@@ -26,35 +33,73 @@ function convertMs(ms) {
   }
 
 const input = document.querySelector('#datetime-picker');
-const timeDays = document.querySelector('[data-days]');
-const timeHours = document.querySelector('[data-hours]');
-const timeMinutes = document.querySelector('[data-minutes]');
-const timeSeconds = document.querySelector('[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
+
+
+const refs = {
+timeDays: document.querySelector('span[data-days]'),
+timeHours: document.querySelector('span[data-hours]'),
+timeMinutes: document.querySelector('span[data-minutes]'),
+timeSeconds: document.querySelector('span[data-seconds]'),
+}
+let intervalId = null;
+let backTime;
+
+startBtn.disabled = true;
+const currentDate = new Date();
 
 const options = {
-    timerId: null,
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-    selectedDates = Date.now(input.value);
-      console.log(selectedDates[0]);
-    },
-  };
+     if (selectedDates[0] - currentDate <= 0) {
+      Notify.failure("Please choose a date in the future");
+    };
+    // if (selectedDates[0] - currentDate > 0) {
+    //   Notiflix.Notify.success('You have selected the date ${backTime}');
+    // };
+    startBtn.disabled = false;
+  }
+}
 
     const fp = flatpickr(input, options);
-    const btnStart = document.querySelector('.js-btnStart');
 
-// btnStart.addEventListener('click', startTimer);
-    input.addEventListener('change', getDayX)
+    startBtn.addEventListener('click', startTimer);
 
-function getDayX() {
-    timerId = setInterval(() => {
-        let backTime = fp.selectedDates[0] - new Date();
-        const { days, hours, minutes, seconds } = convertMs(backTime);
-        // console.log('backTime:', backTime); 
-        console.log('${days} : ${hours} : ${minutes} : ${seconds}'); 
+    function startTimer() {
+intervalId = setInterval (() => {
+  backTime = fp.selectedDates[0] - new Date();
+  const dataUpdateTime = convertMs(backTime);
+  updateFaseTimer(dataUpdateTime) 
+
+  
+
+console.log(backTime);
+
+  if (backTime <= 0) {
+    clearInterval(intervalId);
+    alert("The time has come");
+  }
+}, 1000);
+    }
+
+    function updateFaseTimer({ days, hours, minutes, seconds }){
+      refs.timeDays.textContent = days;
+      refs.timeHours.textContent = hours;
+      refs.timeMinutes.textContent = minutes;
+      refs.timeSeconds.textContent = seconds; 
+  }
+
+//     input.addEventListener('change', getDayX)
+
+// function getDayX() {
+//     timerId = setInterval(() => {
+//         let backTime = fp.selectedDates[0] - new Date();
+//         const { days, hours, minutes, seconds } = convertMs(backTime);
+//         // console.log('backTime:', backTime); 
+//         console.log('${days} : ${hours} : ${minutes} : ${seconds}'); 
 
 // let currentTime = backTime;
 //         let timeDay = currentTime.getDay();
@@ -64,19 +109,15 @@ function getDayX() {
 
 
 
-        if (backTime <= 0) {
-            clearInterval(timerId);
-            console.log(`Interval with id ${timerId} has stopped!`);
-          }
-        }, 1000);
-}
+//         if (backTime <= 0) {
+//             clearInterval(timerId);
+//             return;
+//             // console.log(`Interval with id ${timerId} has stopped!`);
+//           }
+//         }, 1000);
+// }
 
-function updateFaseTimer({ days, hours, minutes, seconds }){
-    timeDays.textContent = '${days}';
-    timeHours.textContent = '${hours}';
-    timeMinutes.textContent = '${minutes}';
-    timeSeconds.textContent = '${seconds}'; 
-}
+
 
 
   
